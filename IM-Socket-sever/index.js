be8b909f641628189a706bae5f822ebcbf416629
我昊 <im.hansking@gmail.com>
2020-07-22 23:15:43 +0800

    前后端联调

--- IM-Socket-sever/index.js
+++ IM-Socket-sever/index.js
@@ -4,19 +4,8 @@ var app = express();
 var sever = app.listen(8082);
 var io = require('socket.io').listen(sever);
 
-io.on('connection', (socket) => {
-    console.log('socket 连接成功');
-
-    // 接受信息
-    socket.on('message', data => {
-        // console.log(data)
-
-        // 广播消息
-        socket.broadcast.emit('gbmsg', data)
-    })
-
-
-});
+// 引入socket.js
+require('./model/socket.js')(io)
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
